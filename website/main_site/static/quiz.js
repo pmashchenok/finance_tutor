f4a919d87c23ce47cfc154a5d6d968afa4a0613c
vkirs229037 <vova.kirsanov.03@mail.ru>
2024-06-27 00:03:58 +0300

    Оформление (почти все) для теста

--- website/main_site/static/quiz.js
+++ website/main_site/static/quiz.js
@@ -4,35 +4,68 @@
 
 var steps = 0;
 var nQuestions = test.length;
-var div = document.getElementsByClassName("question")[0];
+var div = document.getElementsByClassName("question_container")[0];
 var answers = [];
 
 function getQuestion() {
     steps += 1;
     const question = test[steps-1];
-    div.innerHTML = `
-        <h2>№${steps}. ${question.text}</h2>
-        <div class="question_container">
-            <form class="questions">
-                <label><input type="radio" id="button1" name="question" value="1" />1</label>
-                <label><input type="radio" id="button2" name="question" value="2" />2</label>
-                <label><input type="radio" id="button3" name="question" value="3" />3</label>
-                <label><input type="radio" id="button4" name="question" value="4" />4</label>
-            </form>
-            <button class="submit_button" type="button" onclick="getAnswer();">Следующий вопрос</button>
-        </div>
-    `;
+    if (steps < nQuestions) {
+        div.innerHTML = `
+            <h2>№${steps}. ${question.text}</h2>
+            <div class="questions">
+                <label class="question">
+                    <input type="radio" name="question" value="1" />
+                    <span>1</span> 
+                </label>
+                <label class="question"> 
+                    <input type="radio" name="question" value="2" />
+                    <span>2</span>
+                </label>
+                <label class="question"> 
+                    <input type="radio" name="question" value="3" />
+                    <span>3</span>
+                </label>
+                <label class="question"> 
+                    <input type="radio" name="question" value="4" />
+                    <span>4</span>
+                </label>
+            </div>
+            <button class="submit_button" type="button" onclick="getAnswer();">→</button>
+        `;
+    } else {
+         div.innerHTML = `
+            <h2>№${steps}. ${question.text}</h2>
+            <div class="questions">
+                <label class="question">
+                    <input type="radio" name="question" value="1" />
+                    <span>1</span> 
+                </label>
+                <label class="question"> 
+                    <input type="radio" name="question" value="2" />
+                    <span>2</span>
+                </label>
+                <label class="question"> 
+                    <input type="radio" name="question" value="3" />
+                    <span>3</span>
+                </label>
+                <label class="question"> 
+                    <input type="radio" name="question" value="4" />
+                    <span>4</span>
+                </label>
+            </div>
+            <form action="${results_url}" method="GET" name="form">
+                <button class="submit_button" type="submit" name="results" onclick="getAnswer();" value="">Завершить тест</button>
+            </form> 
+        `;
+    }
+
 }
 
 function next() {
     if (steps < nQuestions) {
         getQuestion();
     } else {
-        div.innerHTML = `
-            <form action="${results_url}" method="GET" name="form">
-                <button type="submit" name="results" value="">Перейти к результатам</button>
-            </form> 
-        `;
         document.form.results.value = answers;
     }
 }
